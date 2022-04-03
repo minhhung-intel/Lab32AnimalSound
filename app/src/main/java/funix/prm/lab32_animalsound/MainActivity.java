c7882ff3c1d24801773066d80e8fe6e8b065e8c2
hungnmfx00878 <minh.hung.nguyen@intel.com>
2022-04-04 04:54:05 +0700

    ini

---
+++ app/src/main/java/funix/prm/lab32_animalsound/MainActivity.java
@@ -0,0 +1,14 @@
+package funix.prm.lab32_animalsound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_m001_menu);
+    }
+}
